71c67c48ef58a7da79366cbdb88cd6858b2e5416
Ahmet Tashan <ahmet@tashan.cc>
2023-03-13 00:43:32 +0300

    BoxComponent silinmiş yerine CardComponents eklenmiş olup, düzenlemeler yapılmıştır.

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,13 +1,54 @@
-<template>
-    <BoxComponent/>
-</template>
-
 <script>
 
-import BoxComponent from "@/components/BoxComponent.vue";
+import CardComponents from "@/components/CardComponents";
 
 export default {
     name: 'HomeView',
-    components: {BoxComponent},
+    components: {CardComponents},
+    data() {
+        return {
+            payload: '',
+        }
+    }
 }
 </script>
+
+<template>
+    <b-container fluid>
+
+        <h2 class="fs-2 my-5">Free text conversion tools</h2>
+        <b-row no-gutters>
+            <CardComponents color="green-100"
+                            name="Text (ASCII/ANSI)"
+                            type="text"
+                            :payload="payload"
+                            @update:payload="payload = $event"/>
+            <CardComponents color="green"
+                            name="Binary"
+                            type="binary"
+                            :payload="payload"
+                            @update:payload="payload = $event"/>
+            <CardComponents color="blue-100"
+                            name="Url encode"
+                            type="urlEncode"
+                            :payload="payload"
+                            @update:payload="payload = $event"/>
+            <CardComponents color="blue"
+                            name="Hexadecimal"
+                            type="hexadecimal"
+                            :payload="payload"
+                            @update:payload="payload = $event"/>
+            <CardComponents color="purple"
+                            name="Base64"
+                            type="base64"
+                            :payload="payload"
+                            @update:payload="payload = $event"/>
+            <CardComponents color="purple-100"
+                            name="Decimal"
+                            type="decimal"
+                            :payload="payload"
+                            @update:payload="payload = $event"/>
+        </b-row>
+
+    </b-container>
+</template>
