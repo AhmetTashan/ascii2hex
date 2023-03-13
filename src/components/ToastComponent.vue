22ad6082d78f68984acc7fac0da1ada19332a82a
Ahmet Tashan <ahmet@tashan.cc>
2023-03-13 09:02:39 +0300

    ToastComponent.vue hata gösterme ekranı oluşturuldu, HomeView.vue dosyasında düzenlemeler yapıldı.

---
+++ src/components/ToastComponent.vue
@@ -0,0 +1,41 @@
+<script>
+export default {
+    name: "ToastComponent",
+    emits: ["update:error"],
+    props: {
+        error: {
+            type: Array,
+            required: true
+        }
+    },
+    methods: {
+        close(index) {
+            let error = this.error;
+            // delete error[index];
+            error.splice(index, 1);
+            this.$emit("update:error", error);
+        }
+    }
+}
+</script>
+
+<template>
+    <div class="toast-container position-fixed bottom-0 end-0 p-3">
+
+        <div class="toast fade show align-items-center text-bg-danger border-0" role="alert" aria-live="assertive"
+             aria-atomic="true"
+             v-for="(value, index) in error" :key="index">
+            <div class="d-flex">
+                <div class="toast-body">
+                    {{ value }}
+                </div>
+                <button type="button" class="btn-close btn-close-white me-2 m-auto" aria-label="Close"
+                        @click="close(index)"></button>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,13 +1,20 @@
 <script>
 
-import CardComponents from "@/components/CardComponents";
+import CardComponent from "@/components/CardComponent";
+import ToastComponent from "@/components/ToastComponent";
 
 export default {
     name: 'HomeView',
-    components: {CardComponents},
+    components: {ToastComponent, CardComponent},
     data() {
         return {
             payload: '',
+            error: []
+        }
+    },
+    methods: {
+        addError(error) {
+            this.error.push(error);
         }
     }
 }
@@ -16,39 +23,54 @@ export default {
 <template>
     <b-container fluid>
 
-        <h2 class="fs-2 my-5">Free text conversion tools</h2>
+        <h2 class="fs-2 mt-5 mb-3">Free text conversion tools</h2>
         <b-row no-gutters>
-            <CardComponents color="green-100"
-                            name="Text (ASCII/ANSI)"
-                            type="text"
-                            :payload="payload"
-                            @update:payload="payload = $event"/>
-            <CardComponents color="green"
-                            name="Binary"
-                            type="binary"
-                            :payload="payload"
-                            @update:payload="payload = $event"/>
-            <CardComponents color="blue-100"
-                            name="Url encode"
-                            type="urlEncode"
-                            :payload="payload"
-                            @update:payload="payload = $event"/>
-            <CardComponents color="blue"
-                            name="Hexadecimal"
-                            type="hexadecimal"
-                            :payload="payload"
-                            @update:payload="payload = $event"/>
-            <CardComponents color="purple"
-                            name="Base64"
-                            type="base64"
-                            :payload="payload"
-                            @update:payload="payload = $event"/>
-            <CardComponents color="purple-100"
-                            name="Decimal"
-                            type="decimal"
-                            :payload="payload"
-                            @update:payload="payload = $event"/>
+
+            <CardComponent color="green-100"
+                           name="Text (ASCII/ANSI)"
+                           type="text"
+                           @error="addError"
+                           :payload="payload"
+                           @update:data="payload = $event"/>
+
+            <CardComponent color="green"
+                           name="Binary"
+                           type="binary"
+                           @error="addError"
+                           :payload="payload"
+                           @update:data="payload = $event"/>
+
+            <CardComponent color="blue-100"
+                           name="Url encode"
+                           type="urlEncode"
+                           @error="addError"
+                           :payload="payload"
+                           @update:data="payload = $event"/>
+
+            <CardComponent color="blue"
+                           name="Hexadecimal"
+                           type="hexadecimal"
+                           @error="addError"
+                           :payload="payload"
+                           @update:data="payload = $event"/>
+
+            <CardComponent color="purple"
+                           name="Base64"
+                           type="base64"
+                           @error="addError"
+                           :payload="payload"
+                           @update:data="payload = $event"/>
+
+            <CardComponent color="purple-100"
+                           name="Decimal"
+                           type="decimal"
+                           @error="addError"
+                           :payload="payload"
+                           @update:data="payload = $event"/>
         </b-row>
+
+        <ToastComponent :error="error"
+                        @update:error="error = $event"/>
 
     </b-container>
 </template>
